6e008960ddfa03e361e23099611bba009315f481
Mohammad Ali Khan <mohammad_ali93@live.com>
2014-02-02 03:36:53 +0000

    Added a very basic parser for one type of description. However, data is inconsistent so a much more general method is needed.

--- src/RegistrySAXParser.java
+++ src/RegistrySAXParser.java
@@ -4,6 +4,7 @@ import javax.xml.parsers.SAXParserFactory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.Attributes;
@@ -257,11 +258,45 @@ public class RegistrySAXParser {
 				}
 
 				public void doStuff(String html) {
-					Document doc = Jsoup.parseBodyFragment(html);
-					Element body = doc.body();
-					String words = body.text();
-					//System.out.println(words);
-					//System.out.println(html);
+					Document doc = Jsoup.parse(html);
+					Elements paras = doc.getElementsByTag("p");
+					Elements lists = doc.getElementsByTag("ul");
+					Elements headings = doc.getElementsByTag("b");
+					Document doc2;
+					if (headings.get(0).text().substring(0, 6).equals("Nature")) {
+						String[] nature_of_work = paras.get(0).text().split("-");
+						System.out.println("\t" +nature_of_work[0] + " : " + nature_of_work[1]);
+						String[] reasons = paras.get(4).text().split(" ");
+						String reasons_for_processing = "\t" + headings.get(2).text() + " :";
+						for (int i = 4; i < reasons.length; i++) {
+							reasons_for_processing += " " + reasons[i];
+						}
+						System.out.println(reasons_for_processing);
+						String processed = "\t" + headings.get(3).text() +  " :";
+						doc2 = Jsoup.parse(lists.get(0).html());
+						Elements info_collected = doc2.getElementsByTag("li");
+						for (int i = 0; i < info_collected.size(); i++) {
+							processed += " " + info_collected.get(i).text() + ",";
+						}
+						doc2 = Jsoup.parse(lists.get(1).html());
+						Elements sensitive_info = doc2.getElementsByTag("li");
+						for (int i = 0; i < sensitive_info.size(); i++) {
+							processed += " " + sensitive_info.get(i).text()
+									+ "[SENSITIVE],";
+						}
+						processed = processed.substring(0, processed.length() - 1);
+						System.out.println(processed);
+						String shared_with = "\t" + headings.get(5).text() + " :";
+						doc2 = Jsoup.parse(lists.get(2).html());
+						Elements shared = doc2.getElementsByTag("li");
+						for (int i = 0; i < shared.size(); i++) {
+							shared_with += " " + shared.get(i).text() + ",";
+						}
+						shared_with = shared_with.substring(0, shared_with.length() - 1);
+						System.out.println(shared_with);
+						System.out.println("\t" + headings.get(8).text() + " : "
+								+ paras.get(11).text());
+					}
 				}
 			};
 
--- src/x.java
+++ src/x.java
@@ -12,27 +12,18 @@ public class x {
 		Document doc = Jsoup.parse(input, "UTF-8", "http://example.com./");
 		Elements paras = doc.getElementsByTag("p");
 		Elements lists = doc.getElementsByTag("ul");
+		Elements headings = doc.getElementsByTag("b");
 		Document doc2;
-		if (paras.get(0).text().substring(0, 6).equals("Nature")) {
+		if (headings.get(0).text().substring(0, 6).equals("Nature")) {
 			String[] nature_of_work = paras.get(0).text().split("-");
-			System.out.println(nature_of_work[0] + " : " + nature_of_work[1]);
+			System.out.println("\t" +nature_of_work[0] + " : " + nature_of_work[1]);
 			String[] reasons = paras.get(4).text().split(" ");
-			String reasons_for_processing = "";
-			for (int i = 0; i < 4; i++) {
-				reasons_for_processing += reasons[i] + " ";
-			}
-			reasons_for_processing += ":";
+			String reasons_for_processing = "\t" + headings.get(2).text() + " :";
 			for (int i = 4; i < reasons.length; i++) {
 				reasons_for_processing += " " + reasons[i];
 			}
 			System.out.println(reasons_for_processing);
-
-			String[] info_processed = paras.get(6).text().split(" ");
-			String processed = "";
-			for (int i = 0; i < 4; i++) {
-				processed += info_processed[i] + " ";
-			}
-			processed += ":";
+			String processed = "\t" + headings.get(3).text() +  " :";
 			doc2 = Jsoup.parse(lists.get(0).html());
 			Elements info_collected = doc2.getElementsByTag("li");
 			for (int i = 0; i < info_collected.size(); i++) {
@@ -46,18 +37,15 @@ public class x {
 			}
 			processed = processed.substring(0, processed.length() - 1);
 			System.out.println(processed);
-			doc2 = Jsoup.parse(paras.get(8).html());
-			Elements stuff = doc2.getElementsByTag("b");
-			String shared_with = stuff.text();
+			String shared_with = "\t" + headings.get(5).text() + " :";
 			doc2 = Jsoup.parse(lists.get(2).html());
 			Elements shared = doc2.getElementsByTag("li");
-			shared_with += " :";
 			for (int i = 0; i < shared.size(); i++) {
 				shared_with += " " + shared.get(i).text() + ",";
 			}
 			shared_with = shared_with.substring(0, shared_with.length() - 1);
 			System.out.println(shared_with);
-			System.out.println(paras.get(10).text() + " : "
+			System.out.println("\t" + headings.get(8).text() + " : "
 					+ paras.get(11).text());
 		}
 
