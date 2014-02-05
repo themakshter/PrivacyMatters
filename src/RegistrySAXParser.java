e10ef341ea04f31f7a51408856422280fb6b5d53
Mohammad Ali Khan <mohammad_ali93@live.com>
2014-02-05 00:33:45 +0000

    Writing a more rigid statistics parser as some html parts were passing through the filter

--- src/RegistrySAXParser.java
+++ src/RegistrySAXParser.java
@@ -1,12 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.Attributes;
 
 public class RegistrySAXParser {
 	public static void main(String argv[]) {
@@ -17,6 +23,8 @@ public class RegistrySAXParser {
 			SAXParser saxParser = factory.newSAXParser();
 
 			DefaultHandler handler = new DefaultHandler() {
+				PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+						"stats.txt")));
 				private int recordCount, regNumCount, orgNameCount,
 						companiesHouseCount, postcodeCount, countryCount,
 						foiCount, startDateCount, endDateCount,
@@ -134,27 +142,118 @@ public class RegistrySAXParser {
 						String qName) throws SAXException {
 					switch (qName.toUpperCase()) {
 					case "REGISTRATION":
-						System.out.println("Records : " + recordCount
+						out.println("Register statistics for 08/2013"
+								+ "\nRecords : " + recordCount
 								+ "\nRegistration Numbers : " + regNumCount
 								+ "\nOrganisation Names : " + orgNameCount
-								+ "\nCompanies House Numbers : "
-								+ companiesHouseCount + "\nPostcodes : "
-								+ postcodeCount + "\nCountries : "
-								+ countryCount + "\nFOI Flags : " + foiCount
+								+ "\nCompanies House Numbers : "+ companiesHouseCount 
+								+ "\nPostcodes : "+ postcodeCount 
+								+ "\nCountries : "+ countryCount 
+								+ "\nFOI Flags : " + foiCount
 								+ "\nStart Dates : " + startDateCount
 								+ "\nEnd Dates : " + endDateCount
 								+ "\nExempt Flags : " + exemptFlagCount
 								+ "\nTrading Names : " + tradingNameCount
-								+ "\nUK Contact Flags : " + subjectAccessCount
+								+ "\nUK Contact Flags : " + ukContactCount
+								+ "\nSubject Access Flags : " + subjectAccessCount
 								+ "\nNature of Work Descriptions : " + natureOfWorkCount
-								+ "\nOld Data Formats (Purpose 1...) : " + oldBlobCount
-								+ "\nNew Data Formats (Nature of work...) : " + newBlobCount);
+								+ "\nOld Data Formats (Purpose 1...) : "	+ oldBlobCount
+								+ "\nNew Data Formats (Nature of work...) : "	+ newBlobCount);
+						out.close();
+						System.out.println("done!");
+						break;
+					default:
+						break;
+
 					}
 				}
 
 				public void characters(char ch[], int start, int length)
 						throws SAXException {
 					switch (type) {
+//					case REGISTRATION_NUMBER:
+//						System.out
+//								.println("Organisation\n\tRegistration Number : "
+//										+ new String(ch, start, length));
+//						type = 0;
+//						break;
+//					case ORGANISATION_NAME:
+//						System.out.println("\tName : "
+//								+ new String(ch, start, length));
+//						type = 0;
+//						break;
+//					case COMPANIES_HOUSE_NUMBER:
+//						System.out.println("\tCompanies House Number : "
+//								+ new String(ch, start, length));
+//						type = 0;
+//						break;
+//					case ADDRESS_1:
+//						address += new String(ch, start, length);
+//						type = 0;
+//						break;
+//					case ADDRESS_2:
+//						address += ", " + new String(ch, start, length);
+//						type = 0;
+//						break;
+//					case ADDRESS_3:
+//						address += ", " + new String(ch, start, length);
+//						type = 0;
+//						break;
+//					case ADDRESS_4:
+//						address += ", " + new String(ch, start, length);
+//						type = 0;
+//						break;
+//					case ADDRESS_5:
+//						address += ", " + new String(ch, start, length);
+//						type = 0;
+//						break;
+//					case POSTCODE:
+//						System.out.println(address);
+//						address = "\tAddress : ";
+//						System.out.println("\tPostcode : "
+//								+ new String(ch, start, length));
+//						type = 0;
+//						break;
+//					case COUNTRY:
+//						System.out.println("\tCountry : "
+//								+ new String(ch, start, length));
+//						type = 0;
+//						break;
+//					case FOI:
+//						System.out.println("\tFreedom of Information : "
+//								+ new String(ch, start, length));
+//						type = 0;
+//						break;
+//					case START_DATE:
+//						System.out.println("\tStart of Registration : "
+//								+ new String(ch, start, length));
+//						type = 0;
+//						break;
+//					case END_DATE:
+//						System.out.println("\tEnd of Registration : "
+//								+ new String(ch, start, length));
+//						type = 0;
+//						break;
+//					case EXEMPT_FLAG:
+//						System.out.println("\tExempt from Processing : "
+//								+ new String(ch, start, length));
+//						type = 0;
+//						break;
+//					case TRADING_NAME:
+//						System.out.println("\tTrading names : "
+//								+ new String(ch, start, length));
+//						type = 0;
+//						break;
+//					case UK_CONTACT:
+//						System.out.println("\tContact in UK: "
+//								+ new String(ch, start, length));
+//						type = 0;
+//						break;
+//					case SUBJECT_ACCESS_CONTACT:
+//						System.out.println("\tSubject Access Contact : "
+//								+ new String(ch, start, length));
+//						type = 0;
+//						break;
 					case NATURE_OF_WORK:
 						doStuff(new String(ch, start, length));
 						type = 0;
@@ -167,16 +266,26 @@ public class RegistrySAXParser {
 				public void doStuff(String html) {
 					Document doc = Jsoup.parse(html);
 					Elements paras = doc.getElementsByTag("p");
-					try{
-						String heading = paras.get(0).text().split(" ")[0];
+					ArrayList<String> paragraphs = new ArrayList<String>();
+					for(Element e: paras){
+						if(e.text().length() > 1){
+							paragraphs.add(e.text());
+							break;
+						}
+					}
+					try {
+						String heading = paragraphs.get(0).split(" ")[0];
 						if (heading.equals("Nature")) {
 							newBlobCount++;
 						} else if (heading.equals("Purpose")) {
 							oldBlobCount++;
+						}else{
+							out.println("Neither purpose nor nature of work : " + heading + "\nHTML : " + html);
 						}
-	
-					}catch(IndexOutOfBoundsException e){
-						System.err.println("Faulty nature of work description : " + html);
+
+					} catch (IndexOutOfBoundsException e) {
+						out.println("Faulty nature of work description : "
+										+ html);
 					}
 				}
 			};
