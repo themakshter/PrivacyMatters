2396d3ae7479b67812cce37890ceaf7844a0e693
Mohammad Ali Khan <mohammad_ali93@live.com>
2014-02-27 22:06:53 +0000

    Added list of data controllers to be displayed and also display json of data controller if clicked

--- WebApplication/app/controllers/Application.java
+++ WebApplication/app/controllers/Application.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.codehaus.jackson.JsonNode;
 
 import play.*;
@@ -7,10 +11,13 @@ import play.libs.Json;
 import play.mvc.*;
 import views.html.*;
 import com.mongodb.*;
+import models.*;
 
 
 public class Application extends Controller {
-  
+	private static MongoClient client;
+
+	
     public static Result index() {
         return ok(index.render("Privacy Matters"));
     }
@@ -19,14 +26,62 @@ public class Application extends Controller {
     	String json = "Could not connect";
     	String result = "(could not find)";
     	try{
-    		MongoClient client = new MongoClient("localhost",27017);
-    		DB database = client.getDB("dataControllers");
-    		DBCollection registry = database.getCollection("registry");
+    		DBCollection registry = connectToDB("registry");
     		DBCursor cursor = registry.find();
     		json = cursor.next().toString();
-    		client.close();
-    		JsonNode node = (JsonNode) Json.parse(json);
+    		JsonNode node = Json.parse(json);
     		result = node.findPath("organisationName").getTextValue();
+    		closeDB();
+    	}catch(Exception e){
+    		System.out.println(e);
+    	}
+    	return ok(result);
+    }
+    
+    public static DBCollection connectToDB(String collection) throws UnknownHostException{
+    	   	MongoClientURI dbURI = new MongoClientURI("mongodb://admin:incorrect@ds033629.mongolab.com:33629/data_controllers");
+    		client = new MongoClient(dbURI);
+    		DB database = client.getDB(dbURI.getDatabase());
+    		DBCollection collectionRetrived = database.getCollection(collection);
+    		return collectionRetrived;
+    }
+    
+    public static void closeDB(){
+    	client.close();
+    }
+    
+    public static Result registry(){
+    	ArrayList<RegistryListItem> regList = new ArrayList<RegistryListItem>();
+    	try{
+    		DBObject controller;
+    		String json;
+    		JsonNode node;
+    		DBCollection registry = connectToDB("registry");
+    		DBCursor cursor = registry.find();
+    		while(cursor.hasNext()){
+    			controller = cursor.next();
+    			json = controller.toString();
+    			node = Json.parse(json);
+    			String regNo = node.findPath("registrationNumber").getTextValue();
+    			String name = node.findPath("organisationName").getTextValue();
+    			regList.add(new RegistryListItem(regNo,name));
+    		};
+    		closeDB();
+    	}catch(Exception e){
+    		System.out.println(e);
+    	}
+    	return ok(registry.render(regList));
+    }
+    
+    public static Result dataController(String registrationNumber){
+    	String result = "Data Controller doesn't exist";
+    	try{
+    		DBCollection registry = connectToDB("registry");
+    		BasicDBObject query = new BasicDBObject("registrationNumber",registrationNumber);
+    		DBCursor cursor = registry.find(query);
+    		//JsonNode node = Json.parse(cursor.next().toString());
+    		result = cursor.next().toString();
+    		closeDB();
     	}catch(Exception e){
     		System.out.println(e);
     	}
