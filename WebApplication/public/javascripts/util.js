9c58930536902f595a9ebb1d4977c012f8ad7f54
Mohammad Ali Khan <mohammad_ali93@live.com>
2014-03-10 19:38:09 +0000

    Please don't break now please

--- WebApplication/public/javascripts/util.js
+++ WebApplication/public/javascripts/util.js
@@ -1,18 +1,13 @@
 $(document).ready(function() {
 $(".purpose").click(function() {
-	if($(this).find("span").hasClass('glyphicon glyphicon-plus'))
+	if($(this).find("span").hasClass('glyphicon-plus'))
 	{
-		$(".icon").removeClass("glyphicon glyphicon-minus").addClass("glyphicon glyphicon-plus");
-		$(this).find("span").removeClass("glyphicon glyphicon-plus").addClass("glyphicon glyphicon-minus");
-//		$(this).find(".glyphicon").remove(); 
-//		$(this).find(".panel-title").append('<span class="glyphicon glyphicon-minus"></span>');
+		$(".purpose span").removeClass("glyphicon-minus").addClass("glyphicon-plus");
+		$(this).find("span").removeClass("glyphicon-plus").addClass("glyphicon-minus");
 	}
 	else
 	{   
-		console.log("closing");
-		$(this).find("span").removeClass("glyphicon glyphicon-minus").addClass("glyphicon glyphicon-plus");
-//		$(this).find(".glyphicon").remove(); 
-//		$(this).find(".panel-title").append('<span class="glyphicon glyphicon-plus"></span>');
+		$(this).find("span").removeClass("glyphicon-minus").addClass("glyphicon-plus");
 	}
 });
 });
