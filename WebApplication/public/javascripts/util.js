d9789bfc12cd2315d4fcc114181c790c4367db3e
Mohammad Ali Khan <mohammad_ali93@live.com>
2014-03-10 17:44:05 +0000

    Fixed issues with the icons not changing

--- WebApplication/public/javascripts/util.js
+++ WebApplication/public/javascripts/util.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
-$(".panel-heading").click(function (event) {
+$(".purpose").click(function() {
+	//alert($(this).find("span").length);
+	//if($(this).find("span").length){
 	if($(this).find("span").hasClass('glyphicon glyphicon-plus'))
 	{
+		$(".purpose-title span").remove()
+		$(".purpose-title").append('<span class="glyphicon glyphicon-plus"></span>');
 		$(this).find(".glyphicon").remove(); 
 		$(this).find(".panel-title").append('<span class="glyphicon glyphicon-minus"></span>');
 	}
 	else
-	{      
+	{   
 		$(this).find(".glyphicon").remove(); 
 		$(this).find(".panel-title").append('<span class="glyphicon glyphicon-plus"></span>');
+		
 	}
+	//}
 }); 
 });
